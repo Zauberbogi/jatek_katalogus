dabf81c4990e39f37af4da66c592b169fb3989df
Zauberbogi <72388944+Zauberbogi@users.noreply.github.com>
2025-04-03 20:47:21 +0200

    Update users.sql

--- database/users.sql
+++ database/users.sql
@@ -1,12 +1,12 @@
-INSERT INTO `user_person` (`id`, `name`, `rank`, `email`, `password`) VALUES
-(1, 'Zauber Boglárka', 'Admin', 'zauberboglarkaWP@gmail.com', 'ruberduckies'),
-(2, 'György Péter', 'Admin', 'gyorgy.peter.WP@gmail.com', 'MechaMachines'),
-(3, 'Gipsz Jakab', 'Tester', 'gipszjakab@gmail.com', 'gypszjakib'),
-(4, 'Magneti Marelli', 'Tester', 'MagnetiMarelli@MMWPsuper.com', 'mmm-Magneti'),
-(5, 'Wise', 'Tester', 'P.WiseP@interknot.hk', 'HellaWise'),
-(6, 'Belle', 'Tester', 'P.BelleP@interknot.hk', 'SlayinBelle'),
-(7, 'ARTA', 'Tester', 'ARTA.supergt@yahoo.jp', 'ARTAstic'),
-(8, 'Inner Circle', 'Tester', 'innercircleband@bing.com', 'bobmarleybest'),
-(9, 'Rebbeca Ford', 'User', 'rebbford@blsky.social.com', 'warframelotusmommy'),
-(10, 'Leopard 2A7 MTB', 'User', 'rheinmetallstrongest@germansteel.de', 'DM-63'),
-(11, 'Tiger Shark', 'User', 'adoptmepleaseihaveause@NorthropWP.us', 'imbetterthanthef16');
+INSERT INTO `user_person` (`id`, `name`, `rank`, `email`, `password`, `rented_game_id`, `rental_date`) VALUES
+(1, 'Zauber Boglárka', 'Admin', 'zauberboglarkaWP@gmail.com', 'ruberduckies', NULL, NULL),
+(2, 'György Péter', 'Admin', 'gyorgy.peter.WP@gmail.com', 'MechaMachines', NULL, NULL),
+(3, 'Gipsz Jakab', 'Tester', 'gipszjakab@gmail.com', 'gypszjakib', NULL, NULL),
+(4, 'Magneti Marelli', 'Tester', 'MagnetiMarelli@MMWPsuper.com', 'mmm-Magneti', NULL, NULL),
+(5, 'Wise', 'Tester', 'P.WiseP@interknot.hk', 'HellaWise', NULL, NULL),
+(6, 'Belle', 'Tester', 'P.BelleP@interknot.hk', 'SlayinBelle', NULL, NULL),
+(7, 'ARTA', 'Tester', 'ARTA.supergt@yahoo.jp', 'ARTAstic', NULL, NULL),
+(8, 'Inner Circle', 'Tester', 'innercircleband@bing.com', 'bobmarleybest', NULL, NULL),
+(9, 'Rebbeca Ford', 'User', 'rebbford@blsky.social.com', 'warframelotusmommy', NULL, NULL),
+(10, 'Leopard 2A7 MTB', 'User', 'rheinmetallstrongest@germansteel.de', 'DM-63', NULL, NULL),
+(11, 'Tiger Shark', 'User', 'adoptmepleaseihaveause@NorthropWP.us', 'imbetterthanthef16', NULL, NULL);

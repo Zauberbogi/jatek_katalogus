8ce2f2596ac37328ecc545b6f76339eb8da12ef8
Nezha <onedrivepeter@gmail.com>
2025-04-08 12:52:57 +0200

    Delete database/comments.sql

--- database/comments.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `comments` (`id`,`game_id`,`user_id`,`comment_text`,`comment_date`) VALUES
-(1,1,2,'very good game, me likes.','2024-11-29 20:00'),
-(2,5,1,'szeretem a játékot, jó memoriáim vannak róla','2024-11-30 14:00'),
-(3,5,1,'Gyerekkorom kedvenc versenyzős játéka, mindig vissza jövök és játszok vele.','2025-01-10 13:15:39'),
-(4,9,7,'I dislike this game because of its predatory microtransactions.','2025-01-10 13:41:41')

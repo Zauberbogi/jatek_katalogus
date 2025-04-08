40ff9322218fcd0c7f6e72d2acc4f235b9abb22a
Nezha <onedrivepeter@gmail.com>
2025-04-08 12:29:01 +0200

    Update all-in-one.sql

--- database/all-in-one.sql
+++ database/all-in-one.sql
@@ -24,13 +24,12 @@ INSERT INTO `user_person` (`id`, `name`, `rank`, `email`, `password`, `rented_ga
 (10, 'Leopard 2A7 MTB', 'User', 'rheinmetallstrongest@germansteel.de', 'DM-63', NULL, NULL),
 (11, 'Tiger Shark', 'User', 'adoptmepleaseihaveause@NorthropWP.us', 'imbetterthanthef16', NULL, NULL);
 
+INSERT INTO `ratings` (`id`, `rating`, `game_id`, `user_id`) VALUES
+(1,5,1,2),
+(2,5,3,2);
+
 INSERT INTO `comments` (`id`,`game_id`,`user_id`,`comment_text`,`comment_date`) VALUES
 (1,1,2,'very good game, me likes.','2024-11-29 20:00'),
 (2,5,1,'szeretem a játékot, jó memoriáim vannak róla','2024-11-30 14:00'),
 (3,5,1,'Gyerekkorom kedvenc versenyzős játéka, mindig vissza jövök és játszok vele.','2025-01-10 13:15:39'),
 (4,9,7,'I dislike this game because of its predatory microtransactions.','2025-01-10 13:41:41')
-
-INSERT INTO `ratings` (`id`, `rating`, `game_id`, `user_id`) VALUES
-(1,5,1,2),
-(2,5,3,2)
-
